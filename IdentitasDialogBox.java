08ca35232a8e5e93c239e98aacb1f91bc12dc31e
sandybuana03 <54133789+sandybuana03@users.noreply.github.com>
2020-10-10 17:27:12 +0700

    Create IdentitasDialogBox.java

---
+++ IdentitasDialogBox.java
@@ -0,0 +1,10 @@
+/* program ini untuk menampilkan pesan
+ * dalam suatu kotak dialog
+ */
+import javax.swing.JOptionPane;
+public class IdentitasDialogBox {
+    public static void main(String[] args) {
+        //Menampilkan Kotak Dialog
+        JOptionPane.showMessageDialog(null, "Nama   = Sandy Buana \nHobi      = Ngoding \nAlamat = Tegal");
+    }
+}
