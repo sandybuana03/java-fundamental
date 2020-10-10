5521243addfc733b3f0d9e6f4eaad5c9df172f83
sandybuana03 <54133789+sandybuana03@users.noreply.github.com>
2020-10-10 16:33:24 +0700

    Create HitungLuas.java
    Program menghitung Luas Lingkaran

---
+++ HitungLuas.java
@@ -0,0 +1,26 @@
+//contoh program hitung luas lingkaran
+public class HitungLuas {
+    public static void main(String[]args) {
+        
+      //deklarasi variabel untuk jarijari & luas
+      double jarijari;
+      double luas;
+        
+      //menentukan nilai jarijari
+      jarijari = 20;
+        
+      //menentukan rumus hitung luas lingkaran
+      luas = 3.14*jarijari*jarijari;
+        
+      //tampilkan hasilnya
+      //tampil kalimat
+      System.out.print("Luas lingkaran dengan jari-jari = ");
+      //mengambil nilai jari-jari dari variabel
+      System.out.println(jarijari);
+      //tampil string dan mengambil nilai luas dari variabel
+      System.out.println("adalah = " + luas);
+    }
+}
+
+
+
