4b6df6d501d475d27b8440c9a3ca358f33cfbddd
sandybuana03 <54133789+sandybuana03@users.noreply.github.com>
2020-10-10 17:15:36 +0700

    Create Identitas.java

---
+++ Identitas.java
@@ -0,0 +1,8 @@
+public class Identitas {
+    public static void main (String[] args) {
+        //menampilkan tulisan
+        System.out.println("Nama   = Sandy Buana");
+        System.out.println("Hobi   = Ngoding");
+        System.out.println("Alamat = Tegal");
+    }
+}
