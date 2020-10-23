fdc874e3840723fe0bff000aadb186b420e567d0
sandybuana03 <sandybuana03@gmail.com>
2020-10-23 19:54:14 +0700

    Create Akar.java

---
+++ Akar.java
@@ -0,0 +1,11 @@
+public class Akar {
+    public static void main (String[] args) {
+        int a = 25;
+        double b = Math.sqrt(a); 
+        //secara default tipe data dari Math.sqrt adalah double
+        
+        System.out.println(b);
+        System.out.println(Math.sqrt(144));
+        System.out.println((int)Math.sqrt(121));
+    }
+}
