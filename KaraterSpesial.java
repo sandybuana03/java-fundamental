a33dd738ddff84152757365f47c0292f3fe61bd4
sandybuana03 <sandybuana03@gmail.com>
2020-10-24 09:31:20 +0700

    Create KaraterSpesial.java

---
+++ KaraterSpesial.java
@@ -0,0 +1,21 @@
+public class KaraterSpesial {
+    public static void main (String [] args ) {
+        
+        System.out.println("1. Saya berkata \"MAJU\" "); //petik dua
+        
+        System.out.println("2. Saya berkata \'MAJU\' "); //petik satu
+        
+        System.out.println("3. Saya berkata \t \"MAJU\" "); //tab
+        
+        System.out.println("4. Saya berkata\b \"MAJU\" "); //menghilangkan satu huruf sebelah kiri
+        
+        System.out.println("5. Saya berkata \n \"MAJU\" "); //enter
+        
+        System.out.println("6. Saya berkata \f \"MAJU\" "); //kotak
+        
+        System.out.println("7. Saya berkata \r \"MAJU\" "); //menghilangkan kalimat sebelah kiri
+        
+        System.out.println("8. Saya berkata \\ \"MAJU\" "); //backslash
+        
+    }
+}
