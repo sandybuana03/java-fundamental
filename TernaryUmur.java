52cadc29314c97177b365f7b9e84430d63bb9400
sandybuana03 <54133789+sandybuana03@users.noreply.github.com>
2020-10-29 06:26:06 +0700

    Update TernaryUmur.java

--- TernaryUmur.java
+++ TernaryUmur.java
@@ -9,7 +9,10 @@ public class TernaryUmur {
         
         System.out.println("-----------------------");
         
-        String status = (umur < 5) ? "Balita" : (umur < 13) ? "Anak-Anak" : (umur < 18) ? "Remaja" : (umur < 35) ? "Dewasa" :"Tua";
+        String status = (umur < 5) ? "Balita" : 
+                        (umur < 13) ? "Anak-Anak" : 
+                        (umur < 18) ? "Remaja" : 
+                        (umur < 35) ? "Dewasa" :"Tua";
         
         System.out.println("Umur Anda Adalah " + umur + " Tahun");
         System.out.println("Status Anda adalah " + status);
