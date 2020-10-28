101bb1a289fcaf98f1dad0565bc16893bc01d52b
sandybuana03 <sandybuana03@gmail.com>
2020-10-28 09:27:10 +0700

    Update KarakterSpesial.java

--- KarakterSpesial.java
+++ KarakterSpesial.java
@@ -1,4 +1,4 @@
-public class KaraterSpesial {
+public class KarakterSpesial {
     public static void main (String [] args ) {
         
         System.out.println("1. Saya berkata \"MAJU\" "); //petik dua
