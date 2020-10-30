fffe7e6b7848f63b16786dd783bfd65eb5ffc258
sandybuana03 <sandybuana03@gmail.com>
2020-10-30 15:36:06 +0700

    Create PengenalanVariabel.java

---
+++ PengenalanVariabel.java
@@ -0,0 +1,20 @@
+public class PengenalanVariabel {
+    public static void main(String[] args) {
+        
+        // Mendeklarasikan Variabel dan juga nilainya
+        // tipeData namaVariabel = isi;
+        String nama = "SANDY BUANA";
+        String hp = "+6285727486007";
+        String alamat = "Margadana";
+        int noRumah = 18;
+        int rt = 3;
+        int rw = 5;
+        
+        // Menampilkan & Memanggil Variabel
+        System.out.println("Nama     = " + nama);
+        System.out.println("No HP    = " + hp);
+        System.out.println("Alamat   = " + alamat);
+        System.out.println("No Rumah = " + noRumah);
+        System.out.println("Rt"+rt + "/" +"Rw"+rw);
+    }
+}
