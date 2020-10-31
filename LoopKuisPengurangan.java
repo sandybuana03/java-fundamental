8a5ba882f8d047624be2b34bf00da725c11842f2
sandybuana03 <sandybuana03@gmail.com>
2020-10-31 22:02:28 +0700

    Create LoopKuisPengurangan.java

---
+++ LoopKuisPengurangan.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class LoopKuisPengurangan {
+    public static void main(String[] args){
+        int hitungBenar = 0; // hitung jumlah jawaban benar
+        int hitung = 0; //hitung jumlah pertanyaan
+        long waktuMulai = System.currentTimeMillis();
+        String keluaran = "" ; //kosong
+        
+        Scanner masukan = new Scanner(System.in);
+        
+        System.out.print("Masukkan Banyaknya Pertanyaan = ");
+        final int JUMLAH_PERTANYAAN = masukan.nextInt();
+        
+        while (hitung < JUMLAH_PERTANYAAN) {
+            //1. Menentukan angka acak
+            int angka1 = (int)(Math.random() * 101);
+            int angka2 = (int)(Math.random() * 101);
+            
+            //2. jika angka1 < angka2 maka tukar angka
+            if (angka1 < angka2) {
+                int angkaTukar =angka1;
+                angka1 = angka2;
+                angka2 = angkaTukar;
+            }
+            
+            //3. Meminta untuk menjawab
+            System.out.println("\nBerapakah " + angka1 + " - " + angka2 + " ?");
+            System.out.print("Jawab = ");
+            int jawaban = masukan.nextInt();
+            
+            //4. Tampilkan hasil
+            if (angka1 - angka2 == jawaban) {
+                System.out.println("Anda Benar !");
+                hitungBenar++;
+            }
+            else {
+                System.out.println("Jawaban Anda Salah !");
+                System.out.println("Seharusnya " + (angka1-angka2));
+            }
+            
+            //inkremen hitung
+            hitung++;
+            
+            keluaran += "\n" + angka1 + " - " + angka2 + " = " + jawaban +
+                       ((angka1 - angka2 == jawaban) ? " Benar" : " Salah");
+        } // tutup while
+        
+        long waktuAkhir = System.currentTimeMillis();
+        long waktuUji = waktuAkhir - waktuMulai;
+        
+        System.out.println("------------------------------");
+        System.out.println("Jumlah yang benar adalah " + hitungBenar);
+        System.out.println("Waktu Uji adalah " + waktuUji / 1000 + " detik");
+        System.out.println(keluaran);
+    }
+}
