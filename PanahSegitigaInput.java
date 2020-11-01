e0e790d49c457420cd41ac7a6aa361a26ed50bd4
sandybuana03 <54133789+sandybuana03@users.noreply.github.com>
2020-11-01 22:50:14 +0700

    Update PanahSegitigaInput.java

--- PanahSegitigaInput.java
+++ PanahSegitigaInput.java
@@ -7,7 +7,8 @@ public class PanahSegitigaInput {
         System.out.print("Masukkan Jumlah Bintang Tengah = ");
         int baris = masukan.nextInt();
         int baris2 = baris-1;
-       
+        
+        // for pertama 
         for (int i = 1; i <= baris; i++) { //loop A, akan dieksekusi mengikuti nilai baris
              for (int j = 1; j <= i; j++) { // loop B, dieksekusi mengikuti kondisinya
                  System.out.print("*");     
@@ -15,11 +16,12 @@ public class PanahSegitigaInput {
              System.out.print("\n"); // ikut loop A
          }   // tutup loop A
 
-        for (int x = 1; x <= baris2; x++) { 
-             for (int y = baris2; y >= x; y--) { 
+        // for kedua
+        for (int x = 1; x <= baris2; x++) { // loop C
+             for (int y = baris2; y >= x; y--) { // loop D
                  System.out.print("*"); 
-             } // tutup loop B
+             } // tutup loop C
              System.out.print("\n");
-         }  // tutup loop A
+         }  // tutup loop D
     }
 }
