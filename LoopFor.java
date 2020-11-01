2288a23997dda08f097749c42c6cc90080bf9cd7
sandybuana03 <sandybuana03@gmail.com>
2020-11-01 18:02:26 +0700

    create

---
+++ LoopFor.java
@@ -0,0 +1,7 @@
+public class LoopFor {
+    public static void main (String[] args) {
+        // Eksekusi dilakukan 100x selama i <= 100
+        for (int i = 1; i <= 100 ; i++) // i = 1 adalah aksi awal
+            System.out.println(i + " Sandy Buana"); // Tidak perlu kurung kurawal
+    }                                               // Jika statemen hanya ada satu
+}
---
+++ LoopForBeberapaEkspresi.java
@@ -0,0 +1,10 @@
+public class LoopForBeberapaEkspresi {
+    public static void main (String[] args) {
+        // Eksekusi dilakukan selama penjumlahan i+j <= 200
+        for (int i = 1, j = 1; (i + j) <= 200 ; i++, j++) { // Terdapat beberapa ekspresi
+            System.out.println(i + " Sandy");
+            System.out.println(j + " Buana");
+            System.out.println("----------");
+        }
+    }
+}
